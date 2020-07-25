9720fa8b5bde8115477bd31c59a78b8f6fd826f3
Timoteo Garcia Bertoa <timingb@gmail.com>
2020-07-25 13:22:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -36,8 +36,8 @@ import os
 from glob import glob
 import site 
 
-if os.environ['BOARD'] != 'Ultra96' and os.environ['BOARD'] != 'Pynq-Z1' and os.environ['BOARD'] != 'Pynq-Z2' and os.environ['BOARD'] != 'zcu104':
-	print("Only supported on a Ultra96, Pynq-Z1 or Pynq-Z2 Board")
+if os.environ['BOARD'] != 'Ultra96' and os.environ['BOARD'] != 'Pynq-Z1' and os.environ['BOARD'] != 'Pynq-Z2' and os.environ['BOARD'] != 'ZCU104':
+	print("Only supported on a ZCU104, Ultra96, Pynq-Z1 or Pynq-Z2 Board")
 	exit(1)
 
 setup(
